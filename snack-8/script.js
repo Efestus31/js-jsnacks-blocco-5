658282a4cb020260514797a366de6fbdae1161f2
Efestus31 <biagiohd@gmail.com>
2024-10-16 15:08:53 +0200

    snack 8 risolto usando find

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,16 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studente = students.find(student => student.name === 'Marco Lanci');
+
+let classe;
+
+if(studente){
+  classe = studente.class;
+}else{
+  classe = null;
+}
+
+
+console.log(classe);
