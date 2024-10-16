0c24be95cdecc153439fb8e2b235969dc4cb30d9
Efestus31 <biagiohd@gmail.com>
2024-10-16 14:33:52 +0200

    snack 3 svolto con map e ciclo for

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,18 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+//svoglimento con map
+const numeriAumentati = numbers.map(number => {
+    return number + 1
+})
+
+console.log(numeriAumentati);
+let numAumentati = []
+
+//svolgimento con for
+for(let i = 0; i < numbers.length; i++){
+    numAumentati.push(numbers[i] + 1)
+}
+
+console.log(numAumentati);
