bae6c71dc71da8d1d5a846b2562325b58e6056a6
Efestus31 <biagiohd@gmail.com>
2024-10-16 13:43:53 +0200

    esercizio 1 fatto con forEach e for loop

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//esercizio fatto con for loop
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+//esercizio svolto usando forEach
+names.forEach(function(name){
+    console.log(name);
+    
+})
