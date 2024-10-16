d96b24739f04d0f369321de3dd9a94dd2467834b
Efestus31 <biagiohd@gmail.com>
2024-10-16 14:59:15 +0200

    snack 7 risolto usando filter

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+let student = students.filter( person => person.id == 1);
+
+console.log(student);
